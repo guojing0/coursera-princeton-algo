318e2c5391710388e4f434381be1e4c5a7783f1f
guoj <dev.guoj@gmail.com>
2016-10-10 13:17:52 -0600

    new stack implement

---
+++ src/LinkedListStack.java
@@ -0,0 +1,66 @@
+import java.util.Iterator;
+import java.util.Objects;
+
+public class LinkedListStack<Item> implements Iterable<Item> {
+
+    private Node first = null;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /*
+     * Traditional implementation of stack based on linked lists
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(Item str) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = str;
+        first.next = oldFirst;
+    }
+
+    public Item pop() {
+        Item temp = first.item;
+        first = first.next;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack<Integer> foo = new LinkedListStack<>();
+        foo.push(1);
+        foo.push(2);
+        Iterator bar = foo.iterator();
+
+        while (bar.hasNext()) {
+            Object i = bar.next();
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/ResizingArrayStack.java
@@ -0,0 +1,73 @@
+import java.util.Iterator;
+
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+
+    private Item[] stack;
+    private int len = 0;
+
+    public ResizingArrayStack() {
+        stack = (Item[]) new Object[1];
+    }
+
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = len;
+
+        public boolean hasNext() {
+            return i > 1;
+        }
+
+        public Item next() {
+            return stack[--i];
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /*
+     * Traditional implementation of stack based on linked lists
+     */
+    public boolean isEmpty() {
+        return stack[0] == null;
+    }
+
+    public void push(Item str) {
+        if (len == stack.length) {
+            resize(2 * stack.length);
+        }
+        stack[len++] = str;
+    }
+
+    public Item pop() {
+        Item item = stack[--len];
+        stack[len] = null;
+        if (len > 0 && len < stack.length / 4) {
+            resize(stack.length / 2);
+        }
+        return item;
+    }
+
+    public void resize(int capacity) {
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < len; i++) {
+            temp[i] = stack[i];
+        }
+        stack = temp;
+    }
+
+    public static void main(String[] args) {
+        ResizingArrayStack foo = new ResizingArrayStack();
+        foo.push("hello");
+        foo.push("world");
+        foo.push("you");
+        System.out.println(foo.pop());
+        System.out.println(foo.pop());
+        System.out.println(foo.pop());
+        System.out.println(foo.isEmpty());
+    }
+}
