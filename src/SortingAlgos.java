19b0f32db403bcd52f153ee7d5d794306c5e9e8e
guoj <dev.guoj@gmail.com>
2016-10-12 23:43:46 -0600

    shell sort and shuffle

--- src/SortingAlgos.java
+++ src/SortingAlgos.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Arrays;
 
 public class SortingAlgos {
@@ -36,10 +38,35 @@ public class SortingAlgos {
         }
     }
 
+    public static void shellSort(Comparable[] arr) {
+        int h = 1;
+
+        while (h < arr.length / 3) {
+            h = 3 * h + 1;
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < arr.length; i++) {
+                for (int j = i; j >= h && less(arr[j], arr[j - h]); j -= h) {
+                    swap(arr, j, j - h);
+                }
+            }
+            h /= 3;
+        }
+    }
+
+    public static void shuffle(Comparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            swap(arr, i, StdRandom.uniform(i + 1)); // random number between 0 and i
+        }
+    }
+
     public static void main(String[] args) {
-        Comparable[] foo = {9, 8, 7, 0, 6, 3, 1, 4, 2, 5};
+        Comparable[] foo = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(Arrays.toString(foo));
-        insertionSort(foo);
+        shuffle(foo);
+        System.out.println(Arrays.toString(foo));
+        shellSort(foo);
         System.out.println(Arrays.toString(foo));
     }
 }
