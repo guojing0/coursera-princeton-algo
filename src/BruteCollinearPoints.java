30bc1489af20e98ec1867f5e4057f7636d58b2d7
guoj <dev.guoj@gmail.com>
2016-10-16 19:21:59 -0600

    brute force

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,15 @@
+public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+
+    }
+
+    public LineSegment[] segments() {
+
+    }
+
+}
